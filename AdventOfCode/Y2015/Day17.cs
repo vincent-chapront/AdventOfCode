e392fcbd329ccd7dc46507779997b28f6fc205db
vincent-chapront <vincent.chapront@gmail.com>
2022-12-31 17:32:37 +0100

    2015 17

--- AdventOfCode/Y2015/Day17.cs
+++ AdventOfCode/Y2015/Day17.cs
@@ -1,42 +1,28 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace AdventOfCode.Y2015
 {
     internal class Day17 : GenericDay
     {
-        protected override object Part1()
+        public string Compute1(string[] input, string args)
         {
-            Assert.AreEqual(4, Compute1(Resources.Year2015.Day17Test.ToLines(), 25));
-            var res = Compute1(Resources.Year2015.Day17.ToLines(), 150);
-            Assert.AreEqual(1304, res);
-            return res;
-        }
-
-        protected override object Part2()
-        {
-            Assert.AreEqual(3, Compute2(Resources.Year2015.Day17Test.ToLines(), 25));
-            var res = Compute2(Resources.Year2015.Day17.ToLines(), 150);
-            Assert.AreEqual(18, res);
-            return res;
-        }
-
-        private static long Compute1(string[] input, int volume)
-        {
+            var volume = Convert.ToInt32(args);
             var containers = input.Select(x => int.Parse(x)).OrderByDescending(x => x).ToList();
-            List<int> res = new List<int>();
+            var res = new List<int>();
             Generate(containers, 0, volume, 0, 0, res);
-            return res.Count;
+            return res.Count.ToString();
         }
 
-        private static long Compute2(string[] input, int volume)
+        public string Compute2(string[] input, string args)
         {
+            var volume = Convert.ToInt32(args);
             var containers = input.Select(x => int.Parse(x)).OrderByDescending(x => x).ToList();
-            List<int> res = new List<int>();
+            var res = new List<int>();
             Generate(containers, 0, volume, 0, 0, res);
             var min = res.Min();
-            return res.Count(x => x == min);
+            return res.Count(x => x == min).ToString();
         }
 
         private static void Generate(List<int> containers, int n, int targetVolume, int currentVolume, int numberOfContainerUsed, List<int> res)
